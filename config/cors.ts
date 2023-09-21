41e56f8ecad51c56be468a6bcd693194aa97f924
kabu1 <bensonmk99@gmail.com>
2023-09-20 21:47:37 -0400

    update cors

--- config/cors.ts
+++ config/cors.ts
@@ -48,7 +48,7 @@ const corsConfig: CorsConfig = {
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
     'http://localhost:5173',
-    'https://mybackendagain.onrender.com/',
+    'https://mybackendagain.onrender.com',
   ],
 
   /*
